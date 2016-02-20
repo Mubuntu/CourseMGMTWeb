7c20599f4b765833a6c352d2b4a88ab03d1f77dc
Mubuntu <patrick.a.frison@gmail.com>
2016-02-20 03:57:43 +0100

    invoegen

---
+++ CourseManagementWebAPI/CourseManagementWebAPI/CourseManagementWebAPI/Controllers/CourseController.cs
@@ -0,0 +1,56 @@
+﻿using CM.UI.MVC.WebAPI.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CM.UI.MVC.WebAPI.Controllers
+{
+    public class CourseController : ApiController
+    {
+        private static List<Models.Course> repo;
+
+        static CourseController()
+        {
+            repo = new List<Course>();
+            repo.Add(new Course()
+            {
+                CourseId = 1,
+                Name = ".NET"
+            ,
+                MaxParticipants = 10,
+                Price = 250.94
+            });
+            repo.Add(new Course()
+            {
+                CourseId = 2,
+                Name = "Java"
+            ,
+                MaxParticipants = 15,
+                Price = 359.11
+            });
+        }
+        public IHttpActionResult Get()
+        {
+            if (repo.Count == 0)
+            {
+                return StatusCode(System.Net.HttpStatusCode.NoContent);
+            }
+
+           return Ok(repo);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            Course requestedCourse = repo.Find(c => c.CourseId == id);
+            if (requestedCourse == null)
+                return NotFound();
+
+            return Ok(requestedCourse);
+        }
+    }
+
+}
---
+++ CourseManagementWebAPI/CourseManagementWebAPI/CourseManagementWebAPI/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CM.UI.MVC.WebAPI.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        [StringLength(20)]
+        public string Name { get; set; }
+        public int MaxParticipants { get; set; }
+        public double? Price { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
